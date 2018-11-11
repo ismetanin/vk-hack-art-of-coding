faabb4fe05b3d50270a67a35919a4c04f4f737ef
Gregory Berngardt <gregoryvit@gmail.com>
2018-11-11 03:58:21 +0300

    Added navigation to AR ViewController logic

--- sevcabel/Screens/Map/MapNavigationController.swift
+++ sevcabel/Screens/Map/MapNavigationController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-final class MapNavigationController: UINavigationController {
+class MapNavigationController: UINavigationController {
+
+    private var childARMapViewController: UIViewController?
+
+    private let switcher = AttitudeSwitcher()
 
     // MARK: - Initialization and deinitialization
 
@@ -23,5 +27,99 @@ final class MapNavigationController: UINavigationController {
         navigationBar.isTranslucent = true
         navigationBar.shadowImage = UIImage()
         navigationBar.setBackgroundImage(UIImage(), for: .default)
+        
+        configureAttitudeSwitcher()
+    }
+}
+
+// MARK: - Switcher
+
+extension MapNavigationController {
+    private func configureAttitudeSwitcher() {
+        
+        self.switcher.switchHandler = { type in
+            
+            switch type {
+            case .up:
+                self.showCam()
+            case .down:
+                self.showMap()
+            }
+            
+        }
+        self.switcher.start()
+        
+    }
+    
+    private func makeARVC() -> UIViewController {
+        let arMapVC = ARMapViewController(nibName: "ARMapViewController", bundle: nil)
+        self.childARMapViewController = arMapVC
+        return arMapVC
+    }
+    
+    private func showMap() {
+        self.childARMapViewController?.view.alpha = 0.0
+        self.tabBarController?.hide(vc: childARMapViewController)
+    }
+    
+    private func showCam() {
+        let arVC = { () -> UIViewController in
+            if let arVC = self.childARMapViewController {
+                return arVC
+            }
+            return makeARVC()
+        }()
+        
+        self.tabBarController?.show(vc: arVC)
+    }
+}
+
+extension UIViewController {
+    func transit(from startVC: UIViewController?, destination endVC: UIViewController?, style: UIView.AnimationOptions = .transitionCrossDissolve) {
+        guard let startVC = startVC, children.contains(startVC) else {
+            show(vc: endVC)
+            return
+        }
+        
+        guard let endVC = endVC else {
+            hide(vc: startVC)
+            return
+        }
+        
+        self.addChild(endVC)
+        startVC.willMove(toParent: nil)
+        
+        UIView.transition(from: startVC.view, to: endVC.view, duration: 0.5, options: style) { (final) in
+            endVC.didMove(toParent: self)
+            startVC.removeFromParent()
+        }
+    }
+    
+    func show(vc: UIViewController?) {
+        guard let vc = vc else {
+            return
+        }
+        
+        self.addChild(vc)
+        vc.viewWillAppear(true)
+        view.addSubview(vc.view)
+        vc.view.frame = view.bounds
+        vc.didMove(toParent: self)
+    }
+    
+    func hide(vc: UIViewController?, hideView:((UIView) -> Void)? = nil) {
+        guard let vc = vc, children.contains(vc) else {
+            return
+        }
+        
+        vc.willMove(toParent: nil)
+        
+        if let hideView = hideView {
+            hideView(vc.view)
+        } else {
+            vc.view.removeFromSuperview()
+        }
+        
+        vc.removeFromParent()
     }
 }
